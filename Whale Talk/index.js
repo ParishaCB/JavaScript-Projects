773d3fb49a48b996cde9d83b7855ba5a3838e0da
Parisha Bhagwatishanker <parishacb@gmail.com>
2023-12-20 18:41:36 +0000

    Add files via upload

---
+++ Whale Talk/index.js
@@ -0,0 +1,23 @@
+let input = 'coding is so fun, this is my answer to the Whale Talk challenge';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      if (input[i] === 'e') {
+        resultArray.push('ee');
+      } else if (input[i] === 'u') {
+        resultArray.push('uu');
+      } else {
+        resultArray.push(input[i]);
+      }
+    }
+  }
+}
+
+console.log(resultArray);
+
+let resultString = resultArray.join('').toUpperCase();
+console.log(resultString)
